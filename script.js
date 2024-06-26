59d95d17c97f540ff219e3130045b3c2d8cb3ef6
Victor Koech <vickoch20@gmail.com>
2024-06-26 12:09:08 +0300

    Updated the script and style

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
 const container = document.querySelector('.container');
+const resetButton = document.getElementById('reset-button');
 
 function createGrid(size) {
+    container.innerHTML = ''; 
+    const squareSize = 960
     for (let i = 0; i < size * size; i++) {
         const square = document.createElement('div');
         square.classList.add('square');
+        square.style.width = `${squareSize}px`;
+        square.style.height = `${squareSize}px`;
         square.addEventListener('mouseenter', () => {
             square.style.backgroundColor = 'black';
+
         });
         container.appendChild(square);
     }
 }
+resetButton.addEventListener('click', () => {
+    let size = parseInt(prompt('Enter the number of squares per side for the new grid (max 100):'));
+    if (size > 100) size = 100; // Limit the size to 100
+    if (size && size > 0) {
+        createGrid(size);
+    } else {
+        alert('Please enter a valid number');
+    }
+});
 
 createGrid(16);
